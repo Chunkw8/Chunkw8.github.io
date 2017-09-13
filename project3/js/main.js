81f5a4716eecfdfa99e6c6431afe7392f0efb531
Chunkw8 <Chunkw8@gmail.com>
2017-09-14 01:15:56 +0300

    intial commut

---
+++ project3/js/main.js
@@ -0,0 +1,14 @@
+function calculation() {
+    var num1, num2, num3, result;
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+    
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+    
+    num3 = document.getElementById('n3').value;
+    num3 = parseInt(num3);
+     
+    result = num2/100*num3*num1+num2;
+    document.getElementById('out').innerHTML = result;
+}
