51507cc3ce010304bd58cff16551f674b4653035
Chunkw8 <Chunkw8@gmail.com>
2017-10-17 22:09:13 +0300

    intial commit

--- project3/js/main.js
+++ project3/js/main.js
@@ -1,33 +1,48 @@
-function calculation() {
-    var num1, num2, num3, num4, num5, result;
-    num1 = document.getElementById('n1').value; /* Срок кредита */
-    num1 = parseInt(num1); /* Позволяет отображать только числовые значения */
+ var num1, num2, num3, num4, num5, result;
 
-    num2 = document.getElementById('n2').value; /* Сумма кредита */
-    num2 = parseInt(num2);
+ function checkradio(form, value) {
+ 	//alert("selected: " + $('input[name=r]:checked').val());
+ 	if (value == "1")
+ 		document.getElementById('n3').value = num3 = 23.8;
 
-    num3 = 23.8 / 12; /* Вшитая процентная ставка в год деленая на количество месяцев в году */
-
-    num4 = document.getElementById('n4').value; /* Первоначальный взнос */
-    num4 = parseInt(num4);
-
-    num5 = ((num2 - num4) + (num2-num4) / 100 * 20); /* расчет суммы кредита + 10% с вычитом первоначального взноса */
+ 	else
+ 		document.getElementById('n3').value = num3 = 19;
+ }
 
 
 
+ function calculation1() { //калькуляция для кредита
+ 	num1 = document.getElementById('n1').value; /* Срок кредита */
+ 	num1 = parseInt(num1); /* Позволяет отображать только числовые значения */
 
-    result = num5 / 100 * num3 * num1 + num5; /* расчет результата */
-    document.getElementById('out').innerHTML = result;
-}
+ 	num2 = document.getElementById('n2').value; /* Сумма кредита */
+ 	num2 = parseInt(num2);
 
-function out1() {
-    var rng = document.getElementById('n1'); //ползунок
-    var i1 = document.getElementById('i1'); // поле ввода
-    i1.value = rng.value;
-}
+ 	//num3 = 23.8 / 12; /* Вшитая процентная ставка в год деленая на количество месяцев в году */
 
-function out2() {
-    var rng = document.getElementById('n1'); //ползунок
-    var i1 = document.getElementById('i1'); // поле ввода
-    rng.value = i1.value;
-}
+
+ 	num4 = document.getElementById('n4').value; /* Первоначальный взнос */
+ 	num4 = parseInt(num4);
+
+ 	if (!num4) {
+ 		num4 = 0;
+ 	}
+ 	num5 = ((num2 - num4) + (num2 - num4) / 100 * 13); /* расчет суммы кредита + 13% с вычитом первоначального взноса */
+
+ 	result = num5 / 100 * num3 * num1 + num5; /* расчет результата */
+
+ 	document.getElementById('out').innerHTML = result;
+ }
+
+
+ function out1() {
+ 	var rng = document.getElementById('n1'); //ползунок
+ 	var i1 = document.getElementById('i1'); // поле ввода
+ 	i1.value = rng.value;
+ }
+
+ function out2() {
+ 	var rng = document.getElementById('n1'); //ползунок
+ 	var i1 = document.getElementById('i1'); // поле ввода
+ 	rng.value = i1.value;
+ }
